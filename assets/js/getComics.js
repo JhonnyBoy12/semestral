18ae8627a72e916b79bde41a001ee89223f4061e
Diego Cruz <dashlol45@gmail.com>
2024-05-23 23:04:34 -0400

    AVANZES DE FETCH Y DOMP API

---
+++ assets/js/getComics.js
@@ -0,0 +1,12 @@
+export const getComics = async()=> {
+    fetch('https://apicomics.onrender.com/')
+    .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error('There has been a problem with your fetch operation:', error));
+
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,93 @@
-import { productos } from "./productos.js";
+import { getComics } from "./getComics.js";
+ 
+const enviarDatos =(name,precio,imagen)=> {
+
+    const rutaArchivoHTML = "../comics.html";
+
+    fetch(rutaArchivoHTML)
+        .then(response => response.text())
+        .then(( html ) => {
+
+            const parser= new DOMParser();
+            const doc = parser.parseFromString(html,"text/html");
+
+            const imagePage = doc.getElementById("imagenPage");
+            imagePage.src = imagen;
+
+            const namePage = doc.getElementById("namePage");
+            namePage.textContent = `${name}`;
+
+            const speciesPage = doc.getElementById("precioPage");
+            speciesPage.textContent = `${precio}`;
 
 
+            const nuevoHTML = new XMLSerializer().serializeToString(doc);
 
-
-const createCard =(name,precio,imagen)=> {
-    return`
-    
-        <div class="col mt-2">
-            <div class="card" style="width: 15rem;">
-                <img src="${imagen}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${name} </h5>
-                    <p class="card-text">${precio}</p>
-                    <a href="./batman1.html" class="btn btn-primary">Ver Mas</a>
-                </div>
-            </div>
-        </div>
-    
-`;
+            document.body.innerHTML = nuevoHTML;
+        })
+        .catch((error)=> {
+            console.log(`El error es: ${error}`);
+        })
 }
 
-const comicCardsRow = document.getElementById('comicCardsRow');
-    productos.map((comic)=> {
-        const card = createCard(comic.name, comic.precio, comic.imagen);
-        comicCardsRow.innerHTML += card;
+
+
+
+const createCard = (result = [])=>{
+    let comicsRow = document.getElementById("comicsRow");
+    result.map((result)=> {
+
+        const {series, name, precio, imagen } = result;
+        
+
+        const divCol = document.createElement("div");
+        divCol.classList.add("col", "mt-2");
+        
+    
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.src = image;
+        img.alt = `Imagen de ${name}`;
+        img.classList.add("card-img-top");
+
+        const divBody = document.createElement("div");
+        divBody.classList.add("card-body");
+
+        const title = document.createElement("h5");
+        title.classList.add("card-title");
+        title.textContent = `Nombre: ${name}`;
+
+        const subTitle = document.createElement("p");
+        subTitle.classList.add("card-text");
+        subTitle.textContent = `Nombre: ${species}`;
+
+        
+        const btnVer = document.createElement("button");
+        btnVer.classList.add("btn","btn-primary");
+        btnVer.textContent = "Ver detalles";
+        btnVer.addEventListener("click",()=> {
+            enviarDatos(id,name,precio,imagen);
+        })
+        
+        divBody.appendChild(title);
+        divBody.appendChild(subTitle);
+        divBody.appendChild(subTitle2);
+        divBody.appendChild(btnVer);
+
+        card.appendChild(img);
+        card.appendChild(divBody);
+
+        divCol.appendChild(card);
+
+        personajesRow.appendChild(divCol);
+
     });
+}
 
 
+getComics()
+    .then(data=> createCard(data))
+    .catch(error=> console.log(error));
 
